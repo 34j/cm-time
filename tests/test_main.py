299cb7c01cc4e28621a085748a6e45b0dc4779c0
34j <55338215+34j@users.noreply.github.com>
2022-12-01 13:50:28 +0900

    test: looser delta

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,6 +15,7 @@ from cm_timer import timer, timer_wrapped
 class TestCatchTime(IsolatedAsyncioTestCase):
     wait: float
     wait_after: float
+    delta: float = 0.1
 
     async def test_basic_usage(self):
         with timer() as ct:
@@ -24,7 +25,7 @@ class TestCatchTime(IsolatedAsyncioTestCase):
 
         self.assertNotEqual(ct.elapsed, None)
         assert ct.elapsed is not None
-        self.assertAlmostEqual(ct.elapsed, self.wait, delta=0.04)
+        self.assertAlmostEqual(ct.elapsed, self.wait, delta=self.delta)
 
     def test_not_yet(self):
         with timer() as ct:
@@ -43,12 +44,12 @@ class TestCatchTime(IsolatedAsyncioTestCase):
         logger, stream = self.get_string_logger()
 
         # Use the logger
-        with timer(logger=logger) as ct:
+        with timer(logger=logger, message="{:.3f}"):
             await asyncio.sleep(self.wait)
         await asyncio.sleep(self.wait_after)
 
         # Check the output
-        self.assertEqual(stream.getvalue(), f"Elapsed time: {ct.elapsed:.3f}\n")
+        self.assertAlmostEqual(float(stream.getvalue()), self.wait, delta=self.delta)
 
     def test_wrapped(self):
         logger, stream = self.get_string_logger()
@@ -71,4 +72,4 @@ class TestCatchTime(IsolatedAsyncioTestCase):
         await test()
         await asyncio.sleep(self.wait_after)
 
-        self.assertAlmostEqual(float(stream.getvalue()), self.wait, delta=0.04)
+        self.assertAlmostEqual(float(stream.getvalue()), self.wait, delta=self.delta)
